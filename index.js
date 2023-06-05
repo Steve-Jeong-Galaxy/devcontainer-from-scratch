6a444126b25058af33aaba730a47f61a6647c10d
Galaxy Steve Jeong <galaxy.steve.jeong@gmail.com>
2023-06-05 02:13:29 +0000

    contact added

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res)=> {
     res.send('Hello World abcd')
 })
 
+app.get('/contact', (req, res)=> {
+    res.send('<h1>Contact</h1>')
+})
+
 const PORT = 3000
 app.listen(PORT,()=>{
     console.log('Server is running at port 3000')
